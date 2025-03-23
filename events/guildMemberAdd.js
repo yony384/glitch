872f027c6416861527231b3d6bf78fe5a6990c29
yony384 <66556138+yony384@users.noreply.github.com>
2025-03-23 05:34:44 +0200

    Create guildMemberAdd.js

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,8 @@
+module.exports = async(client, member) => {
+  const Discord = require("discord.js");
+  // const guild = member.guild;
+  // var channel = client.channels.get("548146739601997862");
+  // channel.setName(`${guild.memberCount} members`)
+  // .then(newChannel => console.log(`Channel's new name is ${newChannel.name}`))
+  // .catch(console.error);
+};
