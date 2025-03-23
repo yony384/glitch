a35dda623c0ae23f355732381a9d2a95a46e55bc
yony384 <66556138+yony384@users.noreply.github.com>
2025-03-23 05:38:10 +0200

    Create guildMemberRemove.js

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,19 @@
+module.exports = async(client, member) => {
+    const Discord = require("discord.js");
+//      const guild = member.guild
+//      let channel = guild.channels.find(channel => channel.name === "👋welcome-bye👋");
+//   if(!channel) {
+//     channel = guild.channels.find(channel => channel.name === "certified-hell");
+//     if(!channel) return;
+//      }
+//        let byeembed = new Discord.RichEmbed()
+//        .setThumbnail(member.user.avatarURL)
+//        .setColor("#ff0000")
+//        .setDescription(`😢 ${member.user.tag} Left!😢`)
+//        .addField("Member", `${member.user }`)
+//        .addField("Current Membercount:", `${member.guild.memberCount} members!`)
+//        .setTimestamp()
+
+//        channel.sendEmbed(byeembed);
+//        return;
+};
